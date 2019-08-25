692ca57830aef3eac74bc64621b4debb225db741
Warren Riley <web@wsr2.com>
2019-08-25 04:31:23 -0400

    Latest edits - still in progress

--- mindOfRiley/js/123.js
+++ mindOfRiley/js/123.js
@@ -1,6 +1,84 @@
 import * as THREE from './three.module.js';
 import { CSS3DRenderer, CSS3DObject } from './CSS3DRenderer.js';
 
+var memMoverCamera = null;
+var memMoverOffset = new THREE.Vector3();
+var memMoverRaycaster = new THREE.Raycaster();
+var memMoverPlane = new THREE.Mesh(new THREE.PlaneBufferGeometry(500, 500, 8, 8), new THREE.MeshBasicMaterial({color: 0xffffff}));
+var memMoverSelection = null; 
+var memMoverControls= null;
+var memMoverObjects= null;
+
+var memMover = {
+    init: function(sceneIn, cameraIn, objectsIn, controlsIn = null) {
+      document.addEventListener('mousedown', this.onDocumentMouseDown, false);
+      document.addEventListener('mousemove', this.onDocumentMouseMove, false);
+      document.addEventListener('mouseup', this.onDocumentMouseUp, false);
+  
+      memMoverPlane.visible = false;
+      memMoverCamera = cameraIn;
+      memMoverObjects = objectsIn;
+      memMoverControls = controlsIn;
+      sceneIn.add(memMoverPlane);
+    },
+      
+    onDocumentMouseDown: function (event) {
+      var mouseX = (event.clientX / window.innerWidth) * 2 - 1;
+      var mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
+  
+      // Get 3D vector from 3D mouse position using 'unproject' function
+      var vector = new THREE.Vector3(mouseX, mouseY, 1);
+      vector.unproject(memMoverCamera);
+      memMoverRaycaster.set( memMoverCamera.position, vector.sub( memMoverCamera.position ).normalize() );
+      var intersects = memMoverRaycaster.intersectObjects(memMoverObjects);
+  
+      if (intersects.length > 0) {
+        if (memMoverControls)  
+            memMoverControls.enabled = false;
+        memMoverSelection = intersects[0].object;
+  
+        var intersects = memMoverRaycaster.intersectObject(memMoverPlane);
+        memMoverOffset.copy(intersects[0].point).sub(memMoverPlane.position);
+      }
+    },
+  
+    onDocumentMouseMove: function (event) {
+      event.preventDefault();
+  
+      var mouseX = (event.clientX / window.innerWidth) * 2 - 1;
+      var mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
+  
+      // Get 3D vector from 3D mouse position using 'unproject' function
+      var vector = new THREE.Vector3(mouseX, mouseY, 1);
+      vector.unproject(memMoverCamera);
+  
+      memMoverRaycaster.set( memMoverCamera.position, vector.sub( memMoverCamera.position ).normalize() );
+  
+      if (memMoverSelection) {
+        var intersects = memMoverRaycaster.intersectObject(memMoverPlane);
+        // Reposition the object based on the intersection point with the plane
+        memMoverSelection.position.copy(intersects[0].point.sub(memMoverOffset));
+      } else {
+        // Update position of the plane if need
+        var intersects = memMoverRaycaster.intersectObjects(memMoverObjects);
+        if (intersects.length > 0) {
+          memMoverPlane.position.copy(intersects[0].object.position);
+          memMoverPlane.lookAt(memMoverCamera.position);
+        }
+      }
+    },
+    
+    onDocumentMouseUp: function (event) {
+      if (memMoverControls)
+        memMoverControls.enabled = true;
+      memMoverSelection = null;
+    }
+  };
+  
+
+
+
+
 let camera, scene, renderer;
 
 let dancingMemories = true;
@@ -39,6 +117,8 @@ let MakeEditable = function(edits) {
     editorDiv.appendChild(textURLElement);
     return editorDiv;
 }
+
+let pagesObjects= [];
 
 let Element = function ( id, x, y, z) {
     let div = document.createElement( 'div' );
@@ -223,7 +303,9 @@ function init() {
     ];
 
     memories.forEach(element => {
-        pagesGroup.add( new Element( element, Math.floor(Math.random() * 1000), Math.floor(Math.random() * 1000), Math.floor(Math.random() * 1000)) );        
+        let e = new Element( element, Math.floor(Math.random() * 1000), Math.floor(Math.random() * 1000), Math.floor(Math.random() * 1000));
+        pagesGroup.add( e );
+        pagesObjects.push(e);        
     });
 
     scene.add( pagesGroup );
@@ -234,7 +316,7 @@ function init() {
     // Block iframe events when dragging camera
     let blocker = document.getElementById( 'blocker' );
     blocker.style.display = 'none';
-    memMover.init(scene, camera, pagesGroup);
+    memMover.init(scene, camera, pagesObjects);
 }
 
 let showKeyPressCodes = false;
--- mindOfRiley/js/memories.js
+++ mindOfRiley/js/memories.js
@@ -1,27 +1,22 @@
+var memMoverCamera = null;
+var memMoverOffset = new THREE.Vector3();
+var memMoverRaycaster = new THREE.Raycaster();
+var memMoverPlane = new THREE.Mesh(new THREE.PlaneBufferGeometry(500, 500, 8, 8), new THREE.MeshBasicMaterial({color: 0xffffff}));
+var memMoverSelection = null; 
+var memMoverControls= null;
+var memMoverObjects= null;
+
 var memMover = {
-    plane: null, 
-    selection: null, 
-    offset: null, 
-    raycaster: null,
-    scene: null,
-    camera: null,
-    objects: null,
-    controls: null,
-  
     init: function(sceneIn, cameraIn, objectsIn, controlsIn = null) {
       document.addEventListener('mousedown', this.onDocumentMouseDown, false);
       document.addEventListener('mousemove', this.onDocumentMouseMove, false);
       document.addEventListener('mouseup', this.onDocumentMouseUp, false);
   
-      this.plane = new THREE.Mesh(new THREE.PlaneBufferGeometry(500, 500, 8, 8), new THREE.MeshBasicMaterial({color: 0xffffff}));
-      this.plane.visible = false;
-      this.camera = cameraIn;
-      this.scene = sceneIn;
-      this.objects = objectsIn;
-      this.controls = controlsIn;
-      this.offset = new THREE.Vector3();
-      this.raycaster = new THREE.Raycaster();
-      this.scene.add(this.plane);
+      memMoverPlane.visible = false;
+      memMoverCamera = cameraIn;
+      memMoverObjects = objectsIn;
+      memMoverControls = controlsIn;
+      sceneIn.add(memMoverPlane);
     },
       
     onDocumentMouseDown: function (event) {
@@ -30,17 +25,17 @@ var memMover = {
   
       // Get 3D vector from 3D mouse position using 'unproject' function
       var vector = new THREE.Vector3(mouseX, mouseY, 1);
-      vector.unproject(this.camera);
-      this.raycaster.set( this.camera.position, vector.sub( this.camera.position ).normalize() );
-      var intersects = this.raycaster.intersectObjects(this.objects);
+      vector.unproject(memMoverCamera);
+      memMoverRaycaster.set( memMoverCamera.position, vector.sub( memMoverCamera.position ).normalize() );
+      var intersects = memMoverRaycaster.intersectObjects(memMoverObjects);
   
       if (intersects.length > 0) {
-        if (this.controls)  
-            this.controls.enabled = false;
-        this.selection = intersects[0].object;
+        if (memMoverControls)  
+            memMoverControls.enabled = false;
+        memMoverSelection = intersects[0].object;
   
-        var intersects = this.raycaster.intersectObject(this.plane);
-        this.offset.copy(intersects[0].point).sub(this.plane.position);
+        var intersects = memMoverRaycaster.intersectObject(memMoverPlane);
+        memMoverOffset.copy(intersects[0].point).sub(memMoverPlane.position);
       }
     },
   
@@ -52,28 +47,28 @@ var memMover = {
   
       // Get 3D vector from 3D mouse position using 'unproject' function
       var vector = new THREE.Vector3(mouseX, mouseY, 1);
-      vector.unproject(this.camera);
+      vector.unproject(memMoverCamera);
   
-      this.raycaster.set( this.camera.position, vector.sub( this.camera.position ).normalize() );
+      memMoverRaycaster.set( memMoverCamera.position, vector.sub( memMoverCamera.position ).normalize() );
   
-      if (this.selection) {
-        var intersects = this.raycaster.intersectObject(this.plane);
+      if (memMoverSelection) {
+        var intersects = memMoverRaycaster.intersectObject(memMoverPlane);
         // Reposition the object based on the intersection point with the plane
-        this.selection.position.copy(intersects[0].point.sub(this.offset));
+        memMoverSelection.position.copy(intersects[0].point.sub(memMoverOffset));
       } else {
         // Update position of the plane if need
-        var intersects = this.raycaster.intersectObjects(this.objects);
+        var intersects = memMoverRaycaster.intersectObjects(memMoverObjects);
         if (intersects.length > 0) {
-          this.plane.position.copy(intersects[0].object.position);
-          this.plane.lookAt(this.camera.position);
+          memMoverPlane.position.copy(intersects[0].object.position);
+          memMoverPlane.lookAt(memMoverCamera.position);
         }
       }
     },
     
     onDocumentMouseUp: function (event) {
-      if (this.controls)
-        this.controls.enabled = true;
-      this.selection = null;
+      if (memMoverControls)
+        memMoverControls.enabled = true;
+      memMoverSelection = null;
     }
   };
   
